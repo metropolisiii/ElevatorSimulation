24edd648a05d58adf18e9b0d1c7358306af70269
Jason Kirby <Jason Kirby>
2017-05-24 18:15:43 -0600

    Added move and openclose functions Added labels to elevator and a boolean variable to tell if elevator is open or closed

--- Elevator.js
+++ Elevator.js
@@ -1,19 +1,47 @@
-//Elevator class
 const MAX_FLOOR = 100;
 const MIN_FLOOR = 1;
-	
+const directionEnum = {
+	UP: 1,
+	DOWN: -1
+}
+
+//Elevator class	
 var Elevator = class{
 	//Constructor
-	constructor(){
-		this.floor = MIN_FLOOR; //All elevators will start on the 1st floor
+	constructor(num){
+		this._floor = MIN_FLOOR; //All elevators will start on the 1st floor
+		this._num = num; //Labels the elevator
+		this._isOpen = true; //When the door is closed, the elevator is moving
+	}
+	
+	//Move the elevator once in the direction specified
+	move(direction){
+		// A valid direction must be given
+		if (typeof direction != 'number')
+			return;
+		this._floor += direction;
+		
+		//Report the floor
+		console.log("Elevator "+num+" has moved to floor: "+this.floor);
+	}
+	
+	//Open or close the elevator
+	openclose(action){
+		switch(action){
+			case 'open':
+				this._isOpen = true;
+				break;
+			case 'close':
+				this._isOpen = false;
+				break;
+		}
 	}
 }
 
 //Main
 const NUM_ELEVATORS = 5;
-
 var elevators = [];
 
 for (var i=0; i<NUM_ELEVATORS; i++){
-	elevators[i] = new Elevator();
+	elevators[i] = new Elevator(i);
 }
